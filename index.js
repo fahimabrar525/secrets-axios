2578f1d450ccc00cace279d8fa956ee04d767d6b
fahimabrar525 <fahimabrar525@gmail.com>
2024-08-01 11:15:44 +0600

    initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+import express from "express";
+
+const port = 3000;
+const app = express();
+
+app.use(express.static('public'));
+
+
+app.get('/', async (req, res) => {
+    try{
+        const result = await axios.get("https://secrets-api.appbrewery.com/random");
+        console.log(result);
+        res.render("index.ejs", {secret: result.data.secret, user: result.data.username});
+    }catch (error) {
+        console.log(error.response.data);
+        res.status(500);
+      }
+});
+
+
+app.listen(port, ()=>{
+    console.log(`Server is running on ${port}`);
+});
+
+
